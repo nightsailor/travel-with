c6d4caa8a03edd90c7686261d76a6e9cb38c746c
Night Sailor <sulemanshah432@gmail.com>
2021-09-26 13:32:29 +0000

    homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,39 @@
 import Topbar from "../../components/topbar/Topbar";
 import "./home.css"
+import bot from "./chat-bot.png"
+import travel from "./tour-buddy.jpg"
 
 export default function Home() {
   return (
     <>
       <Topbar />
       <div className="homeContainer">
-        <p>Home Page</p>
+        <div className="homeWrapper">
+            <div className="homeIcon">
+                <img
+                    className="imgIcon"
+                    src={bot}
+                    alt=""
+                    width={400}
+                />
+            </div>
+            <div className="homeText">
+                A Chat Bot that recommends playlist based on the journey.
+            </div>
+        </div>
+        <div className="homeWrapper">
+            <div className="homeText">
+                A Travel Buddy that recommends that recommmends sustainable journey. 
+            </div>
+            <div className="homeIcon">
+                <img
+                    className="imgIcon"
+                    src={travel}
+                    alt=""
+                    width={400}
+                />
+            </div>
+        </div>
       </div>
     </>
   );
